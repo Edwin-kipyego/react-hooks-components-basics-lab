583d45237036a037d34cbb93f6ddfe4cf610c97e
Edwin kipyego <koriredwin95@gmail.com>
2025-04-08 23:37:22 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+// Define the <About> component here
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+      <p>This is the About section of the application.</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
